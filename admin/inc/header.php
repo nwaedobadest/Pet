ef5a86e0c975379560d1a14f189a1f5688d0301c
syrajin <syramaeigpit@gmail.com>
2022-03-05 15:45:37 +0800

    3rd chanegs march 05 2022

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -2,6 +2,10 @@
     <div class="logoSide">
         <p class="petsociety"><img src="../uploads/logo2.png" class="logo">Pet Society Dashboard</p>
     </div>
+    <div class="profile">
+    <img id = "profilePic"/>
+    <p class="adminName">name of the admin</p>
+    </div>
     <div class="timeSide">
    <!--- <input type="text" id="currentDate">-->
     <p id="currentDate"></p>
