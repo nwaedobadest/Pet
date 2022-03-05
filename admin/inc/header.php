b80455dbc1ab0d72dd5d896f19fdadf8f7f2990b
syrajin <syramaeigpit@gmail.com>
2022-03-05 16:22:43 +0800

    last march 05

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -1,13 +1,23 @@
 <div id = "header">
-    <div class="logoSide">
-        <p class="petsociety"><img src="../uploads/logo2.png" class="logo">Pet Society Dashboard</p>
+    <div class="firstlayer">
+         <div class="logoSide">
+             <img src="../uploads/logo2.png" class="logo"> <p class="petsociety">Pet Society Dashboard</p>
+         </div>
+         <div class="profile">
+            <div class="dropbtn">
+                 <img id = "profilePic"/>
+                 <p class="adminName">name of the admin</p>
+            </div>
+             <div class="drop-content">
+                 <p class="logOut">Logout</p>
+                 <p class="gotoUser">User Page</p>
+             </div>
+
+          </div>
+        <div class="timeSide">
+             <p id="currentDate"></p>
+        </div>
     </div>
-    <div class="profile">
-    <img id = "profilePic"/>
-    <p class="adminName">name of the admin</p>
-    </div>
-    <div class="timeSide">
-   <!--- <input type="text" id="currentDate">-->
-    <p id="currentDate"></p>
-    </div>
+
+    
 </div>
