011823bb0478fbdd3873b89508e378d22b549c08
jawDropping <carlosjoshua.official@gmail.com>
2022-03-09 22:41:30 +0800

    user index side UI

--- user/inc/header.php
+++ user/inc/header.php
@@ -17,31 +17,11 @@
     <div id = "search">
         <form method = "get" action = "search.php" enctype="multipart/form-data">
             <input type="text" name = 'user_query' placeholder = "Search products here..">
-            <button id = "search_btn" name = "search">Search</button>
+            <button id = "search_btn" name = "search"><img src = "../uploads/search.svg" class = "searchIcon"></button>
             <button id = "cart_btn"><a href = 'cart.php'>Cart (<?php echo count($_SESSION['cart']); ?>)</a></button>
         </form>
     </div><!-- <End of Search> -->
 
     
-    <div id = 'link'>
-        <ul>
-            <?php
-                if(isset($_SESSION['user_username']))
-                {
-                    // echo "<li>".$_SESSION['user_username']."</li>";
-                    echo "<ul>
-                            <li>".$_SESSION['user_username']."</li>
-                            <li><a href = 'myProfile.php?username=".$_SESSION['user_username']."'>My Profile<a/></li>
-                            <li><a href = 'logout.php'>Log Out</a></li>
-                         </ul>";
-                }
-                else
-                {
-                    echo "<li><a href = 'login.php'>Log In</a></li>";
-                    echo "<li><a href = 'signup.php'>Sign Up</a></li>";
-                    echo "<li><a href = 'Donate'>Donate</a></li>";
-                }
-            ?>
-        </ul>
-    </div><!-- <End of Link> -->
+   <!-- <End of Link> -->
 </div><!-- <End of Header> -->
--- user/inc/navbar.php
+++ user/inc/navbar.php
@@ -7,4 +7,25 @@
                     </ul>
                 </li>
             </ul>
+            <div id = 'link'>
+        <ul>
+            <?php
+                if(isset($_SESSION['user_username']))
+                {
+                    // echo "<li>".$_SESSION['user_username']."</li>";
+                    echo "
+                            <a>".$_SESSION['user_username']."</a>
+                            <a href = 'myProfile.php?username=".$_SESSION['user_username']."'>My Profile<a/>
+                            <a href = 'logout.php'>Log Out</a>
+                         ";
+                }
+                else
+                {
+                    echo "<a href = 'login.php'>Log In</a>";
+                    echo "<a href = 'signup.php'>Sign Up</a>";
+                    echo "<a href = 'Donate'>Donate</a>";
+                }
+            ?>
+        </ul>
+    </div>
         </div><!-- <End of Navbar> -->
