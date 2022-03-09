468cc5c4ba23367552be9ca4ec14682dab151811
jawDropping <carlosjoshua.official@gmail.com>
2022-03-09 16:52:05 +0800

    loginUI

--- admin/cat.php
+++ admin/cat.php
@@ -5,7 +5,7 @@
         <table>
             <tr>
                 <td>Enter Category Name: </td>
-                <td><input type="text" name = "cat_name" /></td>
+                <td style="width:60%"><input type="text" name = "cat_name" /></td>
                 <td> <button name = "add_cat">Add Category</button></td>
             </tr>
         </table>
@@ -18,8 +18,8 @@
     <form method = "POST" enctype = "multipart/form-data">
         <table>
             <tr>
-                <th style="width:10%" >Category Id</th>
-                <th colspan = 3 >Category Name</th>
+                <th style="width:15%" >Category Id</th>
+                <th colspan = 3 style="width:80%" >Category Name</th>
             </tr>
             <tr>
                 <?php
--- user/login.php
+++ user/login.php
@@ -16,26 +16,94 @@
     </head>
     <body>
         <div id ="LoginForm">
-            <h3>Login</h3>
+            <div class="container">
+                <div class="inside">
+                    <div class="logoSide">
+                    <img src="../uploads/logo2.png" class="logo"> <p class="petsociety">Pet Society</p>
+                    </div>
+              
+                <h3>Welcome to Pet Society</h3>
                 <form method = "POST" action="<?php echo $_SERVER['PHP_SELF']; ?>"; enctype = "multipart/form-data">
-                 <table>
-                   <tr>
-                       <td>Enter Username: </td>
-                        <td><input type="text" name = "user_username" /></td>
-                    </tr>
-                     <tr>
-                    <td>Enter Password: </td>
-                   <td><input type="password" name = "user_password" /></td>
-            </tr>
-        </table>
-        <button name = "login_user" id = "login_user">Log In</button>
-        <a href = "/Pet/admin/login.php">Click here to Log in as an Admin</a>
-    </form>
+                 
+                       
+                        <input type="text" name = "user_username" placeholder = "Username"/>               
+                        <input type="password" name = "user_password"  placeholder = "Password"/></br>
+
+                            <button name = "login_user" id = "login_user" >LOGIN</button></br>
+                     <a href = "/Pet/admin/login.php">Log in as an Admin?</a>
+                    </form>
+                </div>
+           
+            </div>
+           
     </div>
     </body>
 
     <style>
-
+        *{
+            font-family: "Varela Round", sans-serif;
+            color: #5a5bf3;
+            
+        }
+        #LoginForm{
+            display: flex;
+            justify-content: center;
+            width: 100vw;
+            
+            
+            
+        }
+        .container{
+            display: flex;
+            justify-content: center;
+            border-radius: 5px;
+            margin-top: 5vh;
+            margin-left: 15%;
+            margin-right: 15%;
+            background: white;
+            width: 60%;
+            height: 70vh;
+            box-shadow:4px 6px 16px 0px rgba(0, 0, 0, 0.2);
+        }
+        .inside{
+            height: 100%;
+            width: 50%;
+            padding-top: 20px; 
+        }
+        input{
+           width: 100%;
+           height: 56px;
+           border-radius: 10px;
+           padding: 10px;
+           text-align: left;
+           outline: none;
+           border: .5px solid #444;
+           margin-top: 15px;
+    
+        }
+        button{
+            width: 100%;
+            height: 56px;
+            border-radius: 25px;
+            margin-top: 10px;
+            outline: none;
+            border: none;
+            color: #888;
+        }
+        .logo{
+            width: 25px;
+             height: 25px;
+        }
+       .logoSide{
+        display: inline-flex;
+        border-bottom: .9px solid black;
+        padding-bottom: 10px;
+        margin-bottom: 20px;
+       }
+       .petsociety{
+           margin-left: 10px;
+           color: #444;
+       }
     </style>
     
 </html>
