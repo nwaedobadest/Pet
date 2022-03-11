80f7a96303ae45f8a274390c5ff763081fa5cf5e
nwaedobadest <71137066+nwaedobadest@users.noreply.github.com>
2022-03-11 10:55:34 -0600

    Update update_cart_qty.php

--- user/update_cart_qty.php
+++ user/update_cart_qty.php
@@ -1,4 +1,13 @@
 <?php
+//since update_cart_quantity would be using session
+//Its neccessary you start a session
+session_start();
+    
+    
     include("inc/function.php");
-    echo update_cart_quantity();
+    
+    //No need to echo
+    //This is because a redirection would be made
+    call_user_func('update_cart_quantity');
+    //echo update_cart_quantity();
 ?>
